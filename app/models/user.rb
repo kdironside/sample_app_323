e09fda45980b94aa69cb3f7ea26904181dbb9105
KD Ironside <kdironside@gmail.com>
2012-05-22 17:39:07 -0500

    add user microposts

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,8 +10,9 @@
 #
 
 class User < ActiveRecord::Base
-  attr_accessible :name, :email, :password, :password_confirmation 
+  attr_accessible :name, :email, :password, :password_confirmation
   has_secure_password
+  has_many :microposts, dependent: :destroy
   
   before_save { |user| user.email = email.downcase }
   before_save :create_remember_token
@@ -24,12 +25,16 @@ class User < ActiveRecord::Base
   validates :password, length: { minimum: 6 }                  
   validates :password_confirmation, presence: true  
   
+  def feed
+    # This is preliminary. See "Following users" for the full implementation.
+    Micropost.where("user_id = ?", id)
+  end  
+  
   private
 
   def create_remember_token
     self.remember_token = SecureRandom.urlsafe_base64
-  end               
-                    
-                    
+  end            
+                                       
 end
 
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -15,7 +15,7 @@ describe User do
 
   before do
     @user = User.new(name: "Example User", email: "user@example.com", 
-                    password: "foobar", password_confirmation: "foobar")
+                     password: "foobar", password_confirmation: "foobar")
   end
 
   subject { @user }
@@ -33,6 +33,10 @@ describe User do
 
   it { should be_valid }
   it { should_not be_admin }
+  
+  it { should respond_to(:authenticate) }
+  it { should respond_to(:microposts) }
+  it { should respond_to(:feed) }
 
   describe "with admin attribute set to 'true'" do
     before { @user.toggle!(:admin) }
@@ -131,4 +135,35 @@ describe User do
     its(:remember_token) { should_not be_blank }
   end
   
+  describe "micropost associations" do
+
+    before { @user.save }
+    let!(:older_micropost) do 
+      FactoryGirl.create(:micropost, user: @user, created_at: 1.day.ago)
+    end
+    let!(:newer_micropost) do
+      FactoryGirl.create(:micropost, user: @user, created_at: 1.hour.ago)
+    end
+
+    it "should have the right microposts in the right order" do
+      @user.microposts.should == [newer_micropost, older_micropost]
+    end
+      it "should destroy associated microposts" do
+      microposts = @user.microposts
+      @user.destroy
+      microposts.each do |micropost|
+        Micropost.find_by_id(micropost.id).should be_nil
+      end
+    end
+    
+    describe "status" do
+      let(:unfollowed_post) do
+        FactoryGirl.create(:micropost, user: FactoryGirl.create(:user))
+      end
+
+      its(:feed) { should include(newer_micropost) }
+      its(:feed) { should include(older_micropost) }
+      its(:feed) { should_not include(unfollowed_post) }
+    end
+  end 
 end
